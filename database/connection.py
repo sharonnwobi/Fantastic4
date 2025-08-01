59ff938f25f55de8b2f472fd6a98d93ac04c5297
uma-2105 <umas1521@icloud.com>
2025-08-01 14:34:59 +0100

    initial flask api

--- database/connection.py
+++ database/connection.py
@@ -1,19 +1,10 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="finance_db"
-)
-
-mycursor = mydb.cursor(dictionary=True) # Use column names
-mycursor.execute("SELECT * FROM stocks")
-result = mycursor.fetchall()
-
-print("General query:")
-for row in result:
-    # print(row)
-    print(f"{row["symbol"]}")
-
-print(f"Database object {mydb}")
+def connect_to_database():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="happy123",
+        database="finance_db"
+    )
+    return mydb
