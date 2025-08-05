5ffdcf86ac3220abbaa033efc8aa7f085071b67a
uma-2105 <umas1521@icloud.com>
2025-08-05 15:18:09 +0100

    updating live data

--- live-data.py
+++ live-data.py
@@ -1,18 +1,11 @@
-from flask import Flask, jsonify
 import yfinance as yf
 
-app = Flask(__name__)
-
-@app.route("/data/<ticker>")
 def get_stock_data(ticker):
     stock = yf.Ticker(ticker)
     data = stock.history(period="1d", interval="1m")
     times = data.index.strftime('%Y-%m-%d %H:%M:%S')
     average_prices = (data['Open'] + data['Close']) / 2
-    return jsonify({
-        "times": times.tolist(),
-        "average_prices": average_prices.tolist()
-    })
+    volume = data['Volume']
+    return times.tolist(), average_prices.tolist()
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+print(get_stock_data("AAPL")) 
