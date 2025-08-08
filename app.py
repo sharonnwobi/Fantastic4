6349d91f463c94d1c0144bdc70e1cd4054ca6922
Viktor <vbadenszki@gmail.com>
2025-08-08 11:03:26 +0200

    app.py bugfix overview page was not loading

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ def overview():
     # results = cursor.fetchall()
 
 
-    results = requests.get("http://localhost:5000/api/overview")
+    results = requests.get("http://localhost:5000/api/overview").json()
     portfolio_data = []
     total_value = 0
     top_stock = None
